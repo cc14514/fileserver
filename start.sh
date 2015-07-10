33fc6f20f847d9cf54f600e59819e65738d8dfec
liangchuan <cc14514@icloud.com>
2015-07-10 14:04:54 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 # python manage.py collectstatic
-uwsgi --http :9091 --socket :9991 --chdir `pwd` --module django_wsgi
+ps -ef |grep 9991|awk '{print $2}'|xargs kill -9
+rm -rf fileserver/*.pyc
+rm -rf service/*.pyc
+uwsgi --processes 16 --threads 4 --buffer-size 32768 --http :9091 --socket :9991 --chdir `pwd` --module django_wsgi --stats /tmp/fileserver.socket &
