cd114c3cef6d0cfe6a2a52635eacb5e76e47af5f
liangchuan <cc14514@icloud.com>
2015-07-11 12:39:51 +0800

    fix

--- start.sh
+++ start.sh
@@ -2,4 +2,11 @@
 ps -ef |grep 9991|awk '{print $2}'|xargs kill -9
 rm -rf fileserver/*.pyc
 rm -rf service/*.pyc
-uwsgi --processes 16 --threads 4 --buffer-size 32768 --http :9091 --socket :9991 --chdir `pwd` --module django_wsgi --stats /tmp/fileserver.socket &
+
+WORKERS=16
+THREADS=10
+LISTEN=2000
+
+uwsgi -l $LISTEN --workers $WORKERS --threads $THREADS --buffer-size 32768 --http :9091 --socket :9991 --chdir `pwd` --module django_wsgi --stats /tmp/fileserver.socket -d /home/appusr/var/log/fileserver.wsgi.log
+
+echo SERVER STARTED.
